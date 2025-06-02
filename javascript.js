03eac33e833994e3d621c0a116c4a22e75f961e1
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-06-02 20:33:57 +0200

    create cord to index and index to cords functions

--- javascript.js
+++ javascript.js
@@ -1,13 +1,46 @@
 const container = document.querySelector("#container");
 
-fillCells();
+const cells = []; 
+
+//----- Start -----//
+beginPlay();
+function beginPlay(){
+
+
+
+    fillCells();
+    console.log(cells)
+
+    cells.at(cordsToIndex(2,15)).style.backgroundColor = "yellow"
+}
+
+//----- Tick -----//
+function tick(){
+
+}
 
 function fillCells(){
     for(let x = 0; x < 16; x++){
         for(let y = 0; y < 16; y++){
             const cell = document.createElement("div");
-            cell.classList = "cell";
+            cell.classList = `cell`;
+            cells.push(cell);
             container.appendChild(cell);
         }
     }
+}
+
+function createSnake(){
+}
+
+function indexToCords(value){
+    const rowCalc = value % 16 + "";
+    const x = parseFloat(rowCalc.at(0)) - 1;
+    const y = value - x * 16;
+
+    return [x,y];
+}
+
+function cordsToIndex(x, y){
+    return x * 16 + y;
 }
